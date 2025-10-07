2cc3ab6ee00f26da806d5460442628bb998817e5
farrahpepino <idcfarrah@gmail.com>
2025-10-07 12:37:52 -0400

    added chat-list and navbar ui

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../Components/navbar/navbar';
+import { ChatRoom } from '../Components/chat-room/chat-room';
+import { ChatList } from '../Components/chat-list/chat-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, ChatList, ChatRoom],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
