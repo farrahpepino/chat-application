caea0472de4c10a33698a0925ee0764e195d3e4d
farrahpepino <idcfarrah@gmail.com>
2025-10-07 21:39:56 -0400

    finished ui for the main feature

--- client/src/Components/chat-room/chat-room.ts
+++ client/src/Components/chat-room/chat-room.ts
@@ -1,11 +1,129 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+interface Message{
+  sender: string;
+  receipient: string;
+  content: string;
+  createdAt: string;
+}
 
 @Component({
   selector: 'app-chat-room',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-room.html',
   styleUrl: './chat-room.css'
 })
 export class ChatRoom {
 
+  messages: Message[] =[
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "can you please buy me iced chocolate?",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "aisha",
+      receipient: "farrah",
+      content: "okay, but cook meatballs",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "fine",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "can you please buy me iced chocolate?",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "aisha",
+      receipient: "farrah",
+      content: "okay, but cook meatballs",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "fine",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "can you please buy me iced chocolate?",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "aisha",
+      receipient: "farrah",
+      content: "okay, but cook meatballs",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "fine",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "can you please buy me iced chocolate?",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "aisha",
+      receipient: "farrah",
+      content: "okay, but cook meatballs",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "fine",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "can you please buy me iced chocolate?",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "aisha",
+      receipient: "farrah",
+      content: "okay, but cook meatballs",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "fine",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "can you please buy me iced chocolate?",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "aisha",
+      receipient: "farrah",
+      content: "okay, but cook meatballs",
+      createdAt: "October 7, 2025"
+    },
+    {
+      sender: "farrah",
+      receipient: "aisha",
+      content: "okieee",
+      createdAt: "October 7, 2025"
+    }
+
+  ]
 }
