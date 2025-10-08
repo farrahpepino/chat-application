427a3a5416e7d74621045b79a2b7b64fc1c93ff8
farrahpepino <idcfarrah@gmail.com>
2025-10-08 10:59:51 -0400

    fixed errors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Hero } from '../Components/hero/hero';
+import { Login } from '../Components/Auth/login/login';
+import { Register } from '../Components/Auth/register/register';
+import { Profile } from '../Components/profile/profile';
+import { ChatList } from '../Components/chat-list/chat-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Hero
+    },
+    {
+        path: '',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Register
+    },
+    {
+        path: 'profile',
+        component: Profile
+    },
+    {
+        path: 'chats',
+        component: ChatList
+    },
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,15 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from '../Components/navbar/navbar';
-import { ChatRoom } from '../Components/chat-room/chat-room';
-import { ChatList } from '../Components/chat-list/chat-list';
-import { Profile } from '../Components/profile/profile';
-import { Login } from '../Components/Auth/login/login';
-import { Register } from '../Components/Auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, ChatList, ChatRoom, Profile, Login, Register],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
